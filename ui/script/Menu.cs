722ea54929cd76a495a645bfeffbe8b8e9ef352e
lursz <lukoszr@gmail.com>
2024-01-11 20:41:47 +0100

    Main menu
    Created main menu and map menu, connected the buttons

---
+++ ui/script/Menu.cs
@@ -0,0 +1,36 @@
+using Godot;
+
+namespace MoonLander.ui.script;
+
+public partial class Menu : Control
+{
+	public override void _Ready()
+	{
+	}
+
+	public override void _Process(double delta)
+	{
+	}
+
+	public void _on_play_ai_pressed()
+	{
+		GetTree().ChangeSceneToFile("res://scenes/main/main.tscn");
+	}
+
+	public void _on_play_human_pressed()
+	{
+		GetTree().ChangeSceneToFile("res://scenes/main/main.tscn");
+	}
+	public void _on_choose_map_pressed()
+	{
+		GetTree().ChangeSceneToFile("res://ui/choose_map.tscn");
+	}
+
+
+	public void _on_quit_pressed()
+	{
+		GetTree().Quit();
+	}
+
+
+}
